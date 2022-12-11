b0c67598b1f2074a75d0992f4dfc6c385ec82e95
Tiago de Oliveira <tiagodeoliveiravillalva@gmail.com>
2022-12-11 20:36:49 -0300

    Criacao do Projeto Tela de Login

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', /*busca essa tag lá do body da pagina index.html*/
+  templateUrl: 'app.component.html' /*renderiza nosso app.component.html*/
+    })
+export class AppComponent {
+  title = 'login';
+}
